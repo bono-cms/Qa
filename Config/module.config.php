6e1a685ab01b72c2218a65c1e5898d336e32ee7f
David Yang <daworld.ny@gmail.com>
2016-08-02 21:57:32 +0500

    Tweak for module manager

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,11 +1,10 @@
 <?php
 
 return array(
-    'module' => array(
-        'name' => 'Questions and Answers',
-        'route' => 'Qa:Admin:Qa@gridAction',
-        'icon' => 'fa fa-question fa-5x',
-        'order' => 1,
-        'description' => 'QA module allows you to make Questions and Answers page on your site',
-    )
+ 'name' => 'Qa',
+ 'caption' => 'Questions and Answers',
+ 'route' => 'Qa:Admin:Qa@gridAction',
+ 'icon' => 'fa fa-question fa-5x',
+ 'order' => 1,
+ 'description' => 'QA module allows you to make Questions and Answers page on your site',
 );
