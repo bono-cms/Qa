b10f359dce98718138ed4c36be5d8d42d70820ba
dave-yang <daworld.ny@gmail.com>
2018-12-17 19:48:58 +0500

    Minor fixes. Use shared action partial

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,7 +4,6 @@ return array(
  'name' => 'Qa',
  'caption' => 'Questions and Answers',
  'route' => 'Qa:Admin:Qa@gridAction',
- 'icon' => 'fa fa-question fa-5x',
  'order' => 1,
  'description' => 'QA module allows you to make Questions and Answers page on your site',
 );
